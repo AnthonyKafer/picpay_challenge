3350fc161c31107fb597e1ff5c6c9943cae96ed1
Anthony <anthony.lbarros@cardway.com.br>
2025-10-30 12:15:18 -0300

    feat: basic setup

---
+++ Tests/MSTestSettings.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.CompilerServices;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+[assembly: InternalsVisibleTo("Tests")]
