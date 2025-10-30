0362b4be1cce127a88dfc4094caed23d4a0b591f
Anthony <anthony.lbarros@cardway.com.br>
2025-10-30 16:31:35 -0300

    feat: tests and small fixes at helpers

---
+++ Tests/HelpersTests/Sanitizer/SanitizerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace picpay_challenge.Helpers
+{
+    [TestClass]
+    public class SanitizerTests
+    {
+        [TestMethod]
+        public void SanitizerOnlyDigitsTests()
+        {
+            string dirtyString = "$.14520-cfafkahfaj/12:42152fafavvbas";
+
+            Assert.AreEqual(Sanitizer.OnlyDigits(dirtyString), "145201242152");
+        }
+    }
+
+}
--- Tests/HelpersTests/Validators/ValidatorTests.cs
+++ Tests/HelpersTests/Validators/ValidatorTests.cs
@@ -7,14 +7,34 @@ namespace picpay_challenge.Helpers.Validators
         [TestMethod]
         public void CPFvalidatorTest()
         {
-            string validCPF = "";
-            string invalidCPF = "";
+            string validCPFWithMask = "000.709.200-87";
+            string validCPFWithoutMask = "00070920087";
 
-            var validCPFResult = Validator.IsValidCPF(validCPF);
-            var invalidCPFResult = Validator.IsValidCPF(invalidCPF);
+            string invalidCPFWithMask = "123.456.789-00";
+            string invalidCPFWithoutMask = "12345678900";
 
-            Assert.IsTrue(validCPFResult);
-            Assert.IsFalse(invalidCPFResult);
+            Assert.IsTrue(Validator.IsValidCPF(validCPFWithMask));
+            Assert.IsTrue(Validator.IsValidCPF(validCPFWithoutMask));
+
+
+            Assert.IsFalse(Validator.IsValidCPF(invalidCPFWithMask));
+            Assert.IsFalse(Validator.IsValidCPF(invalidCPFWithoutMask));
+        }
+
+        [TestMethod]
+        public void CNPJValidatorTest()
+        {
+            string validCNPJWithMask = "63.682.686/0001-68";
+            string validCNPJWitouthMask = "63682686000168";
+
+            string invalidCNPJWithMask = "12.345.678/0001-00";
+            string invalidCNPJWithoutMask = "12345678000100";
+
+            Assert.IsTrue(Validator.IsValidCNPJ(validCNPJWithMask));
+            Assert.IsTrue(Validator.IsValidCNPJ(validCNPJWitouthMask));
+
+            Assert.IsFalse(Validator.IsValidCNPJ(invalidCNPJWithMask));
+            Assert.IsFalse(Validator.IsValidCNPJ(invalidCNPJWithoutMask));
         }
     }
 }
